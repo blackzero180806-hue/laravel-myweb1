88e2c2fc7151ef55d19c2fe3678a6071b65efed1
blackzero180806-hue <blackzero180806@gmail.com>
2026-05-19 14:25:56 +0700

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Ho va ten: Nguyen Duc Trong</h3>
+    <h3>MSSV: 2124110224</h3>
+</body>
+</html>
